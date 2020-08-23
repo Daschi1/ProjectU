a773d29ae20485a0fe994b729a956fec24fc2a95
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2020-08-23 21:06:30 +0200

    Initial commit

---
+++ src/main/java/de/daschi/projectu/ProjectU.java
@@ -0,0 +1,10 @@
+package de.daschi.projectu;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ProjectU implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/daschi/projectu/client/ProjectUClient.java
@@ -0,0 +1,13 @@
+package de.daschi.projectu.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ProjectUClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
